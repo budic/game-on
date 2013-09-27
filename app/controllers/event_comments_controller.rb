7ec461018e1233efa7e17b5180bb4ead10f827f2
Pete Budic <pete.budic@gmail.com>
2013-09-27 21:03:11 +0000

    comment formatting

--- app/controllers/event_comments_controller.rb
+++ app/controllers/event_comments_controller.rb
@@ -6,15 +6,17 @@ class EventCommentsController < ApplicationController
     @event_comment.event_id = @event.id
     
     respond_to do |format|
-      if @event_comment.save   
-        format.js 
-        format.html { redirect_to @event, notice: 'Comment added.' }
-        format.json { render action: 'show', status: :created, location: @event }
-      else
-        format.js
-        format.html { render action: 'show' }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+      if @event_comment.comment && @event_comment.comment != ""
+        if @event_comment.save   
+          format.js 
+          format.html { redirect_to @event, notice: 'Comment added.' }
+          format.json { render action: 'show', status: :created, location: @event }
+        else
+          format.js
+          format.html { render action: 'show' }
+          format.json { render json: @event.errors, status: :unprocessable_entity }
+        end
+      end  
     end 
     
     
